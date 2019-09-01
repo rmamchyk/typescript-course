8c493bebd604813a80c6caadc328bf3dfc97693f
rmamchyk <roman.mamchyk@gmail.com>
2019-09-02 02:23:53 +0300

    display loading while todos are loading from the server

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -9,9 +9,26 @@ interface AppProps {
   deleteTodo: typeof deleteTodo;
 }
 
-export class _App extends React.Component<AppProps> {
+interface AppState {
+  fetching: boolean;
+}
+
+export class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { fetching: false };
+  }
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({ fetching: true });
   }
 
   onTodoClick = (id: number): void => {
@@ -32,6 +49,7 @@ export class _App extends React.Component<AppProps> {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch</button>
+        {this.state.fetching ? 'Loading...' : null}
         {this.renderList()}
       </div>
     )
